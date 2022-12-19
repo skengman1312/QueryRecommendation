6df9e4456aa7297caf4c34d95054d7ba4f33a624
skengman1312 <dart.murtas@yahoo.it>
2022-12-19 19:39:55 +0100

    switched form random to np.random

--- datagen/generator.py
+++ datagen/generator.py
@@ -10,10 +10,13 @@ class DataSet:
 
     def __init__(self, n_entries, n_attributes, attribute_variations):
         attr_vals = [f"value_{i}" for i in range(attribute_variations)]  # the values for all the attributes
-        d = {f"attr_{i}": np.random.choice(attr_vals, size=n_entries) for i in range(n_attributes)}
+        #p = np.random.normal(loc=5, scale=1, size=attribute_variations) # does not work yet
+        print(p)
+        d = {f"attr_{i}": np.random.choice(attr_vals, size=n_entries,p = p) for i in range(n_attributes)}
         df = pd.DataFrame(d)
-        print(df)
+        self.table = df
 
 
 if __name__ == "__main__":
-    d = DataSet(n_entries=10, n_attributes=5, attribute_variations=6)
+    d = DataSet(n_entries=10000, n_attributes=5, attribute_variations=6)
+    print(d.table["attr_0"].value_counts())
