3309e5f578b8553e9284c12b6b95c6f21f2f76f5
skengman1312 <dart.murtas@yahoo.it>
2022-12-19 18:30:38 +0100

    switched form random to np.random

--- datagen/generator.py
+++ datagen/generator.py
@@ -1,18 +1,19 @@
-import random
 import pandas as pd
+import numpy as np
+
 
 class DataSet:
     """
     First class for the generation of a dataset, for now only categorical variables
     all the attributes have the same number of discrete values
     """
+
     def __init__(self, n_entries, n_attributes, attribute_variations):
         attr_vals = [f"value_{i}" for i in range(attribute_variations)]  # the values for all the attributes
-        d = {f"attr_{i}": random.choices(attr_vals, k= n_entries) for i in range(n_attributes)}
+        d = {f"attr_{i}": np.random.choice(attr_vals, size=n_entries) for i in range(n_attributes)}
         df = pd.DataFrame(d)
         print(df)
 
 
-
 if __name__ == "__main__":
-            d = DataSet(n_entries=10, n_attributes= 5, attribute_variations=6)
+    d = DataSet(n_entries=10, n_attributes=5, attribute_variations=6)
