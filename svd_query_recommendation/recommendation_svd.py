43b2ed3ea1b2e05935a4b1a76881744a2929c1d6
Pietro <dart.murtas@yahoo.it>
2023-01-20 19:35:34 +0100

    first reccomenadtion

--- svd_query_recommendation/recommendation_svd.py
+++ svd_query_recommendation/recommendation_svd.py
@@ -24,7 +24,7 @@ def rec(fm):
     np.random.seed(1312)
     u, s, vh = np.linalg.svd(fm, full_matrices=False)
     user_0_concepts = np.where(u[0] > 0)[0]
-    user_0_concepts = u[0].argpartition(-3)[-3:]
+    user_0_concepts = u[0].argpartition(3)[3:]
     user_O_queries = {i: vh[i].argpartition(-5)[-5:] for i in user_0_concepts}
     print(user_0_concepts)
     print(user_O_queries)
