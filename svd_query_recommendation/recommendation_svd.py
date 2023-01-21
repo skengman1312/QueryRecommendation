714d7d8dad57a8a0929c9b74db679eb870853f35
Pietro <dart.murtas@yahoo.it>
2023-01-21 17:34:27 +0100

    First functional generation of novel queries

--- svd_query_recommendation/recommendation_svd.py
+++ svd_query_recommendation/recommendation_svd.py
@@ -1,10 +1,13 @@
 import numpy as np
+import pandas as pd
 from numpy.linalg import svd
 
 
 class recco:
 
     def __init__(self, um):
+        self._res_counts = None
+        self.top_res = None
         self.top_q = None
         self.um = um
         np.random.seed(1312)
@@ -12,10 +15,37 @@ class recco:
             um.fill()
         self.u, self.s, self.vh = np.linalg.svd(um.filled_matrix, full_matrices=False)
 
-
     def get_top_q(self, k):
 
         self.top_q = {i: self.vh[i].argpartition(-k)[-k:] for i in range(self.u.shape[0])}
+        pd.concat([self.um.dataset.query(self.um.queries[q]) for q in self.top_q[10]])
+        self.top_res = [pd.concat([self.um.dataset.query(self.um.queries[q]) for q in self.top_q[i]])
+                        for i in range(len(self.top_q))]
+        self._res_counts = {i: self.top_res[i].value_counts()[:5].reset_index() for i in range(len(self.top_q))}
+
+        def generate_query(cdf):
+            """
+            count data frame
+            :param cdf:
+            """
+            pert = 1
+            while True:
+                cdf[0] *= pert
+                print(pert)
+                print(cdf)
+
+                q = list()
+                for attr in cdf.columns[:-1]:  # excluding the = index of the counts
+                    d = pd.Series({v: cdf[0][cdf[attr] == v].sum() for v in cdf[attr].unique()})
+                    if d.max() > d.sum() / 2:
+                        q.append([attr, "==", d.idxmax()])
+
+                yield q
+                pert = [np.random.uniform(0.2, 1.8) for _ in range(cdf.shape[0])]
+
+        g = generate_query(self._res_counts[0])
+        for _ in range(10):
+            print(next(g))
 
     # def top_queries_per_concept(self):
 
--- testing.py
+++ testing.py
@@ -55,16 +55,16 @@ def full_matrix_test(um):
 
 if __name__ == "__main__":
 
-    d = DataSet(n_entries=100000, n_discrete_attributes=5, n_continuous_attributes=0, discrete_attribute_variations=8)
-    d.save_csv("./discreate_small/dataset.csv")
-    um = UtilityMatrix(d, 2000, 50, 1000)
-    um.fill()
-    um.export_csv("./discreate_small/")
+    # d = DataSet(n_entries=100000, n_discrete_attributes=5, n_continuous_attributes=0, discrete_attribute_variations=8)
+    # d.save_csv("./discreate_small/dataset.csv")
+    # um = UtilityMatrix(d, 2000, 50, 1000)
+    # um.fill()
+    # um.export_csv("./discreate_small/")
 
     #fm, _ = SVT(um.ratings, max_iter=1500)
 
 
-    # um = UtilityMatrix.from_dir("./discreate_small/")
+    um = UtilityMatrix.from_dir("./discreate_small/")
     # um.fill()
     # um.export_csv("./discreate_small/")
     # print(full_matrix_test(um))
@@ -80,4 +80,4 @@ if __name__ == "__main__":
     # print(fm)
 
 
-    print(full_matrix_test(um))
+     # print(full_matrix_test(um))
