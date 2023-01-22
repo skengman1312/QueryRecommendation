8378413e606eb7a25ae7c7159d3af3e45099ec39
Pietro <dart.murtas@yahoo.it>
2023-01-22 17:15:38 +0100

    Implementation of a better recommendation method (V2) based also on user ratings an not only SVD info

--- svd_query_recommendation/recommendation_svd.py
+++ svd_query_recommendation/recommendation_svd.py
@@ -24,7 +24,7 @@ class QSRS:
         :param k:
         """
         self.top_q = {i: self.vh[i].argpartition(-k)[-k:] for i in range(self.u.shape[0])}
-        pd.concat([self.um.dataset.query(self.um.queries[q]) for q in self.top_q[10]])
+        # pd.concat([self.um.dataset.query(self.um.queries[q]) for q in self.top_q[10]])
         self.top_res = [pd.concat([self.um.dataset.query(self.um.queries[q]) for q in self.top_q[i]])
                         for i in range(len(self.top_q))]
         self._res_counts = {i: self.top_res[i].value_counts()[:5].reset_index() for i in range(len(self.top_q))}
@@ -33,7 +33,7 @@ class QSRS:
 
     def recommendation(self, user_id, length: int):
         """
-
+        Recommend taking into account the most associated queries for concepts mostly associated to the user
         :param length: number of queries to be recommended
         :param user_id: user to get the recommendation for
         """
@@ -65,6 +65,58 @@ class QSRS:
             q = next(generators[i % 3])
             if q not in qq:
                 qq.append(q)
+            i += 1
+        return qq
+
+    def recommendationV2(self, user_id, length: int):
+        """
+        Recommend taking into account the top queriesas rated by the user for concepts mostly associated to the user
+        :param length: number of queries to be recommended
+        :param user_id: user to get the recommendation for
+        """
+
+        def generate_query(cdf):
+            """
+            count data frame
+            :param cdf:
+            """
+            pert = 1
+            while True:
+                cdf[0] *= pert
+
+                q = list()
+                for attr in cdf.columns[:-1]:  # excluding the = index of the counts
+                    d = pd.Series({v: cdf[0][cdf[attr] == v].sum() for v in cdf[attr].unique()})
+                    if d.max() > d.sum() / 2:
+                        q.append([attr, "==", f'"{d.idxmax()}"'])
+
+                yield q
+                pert = [np.random.uniform(0.2, 1.8) for _ in range(cdf.shape[0])]
+
+        if not self._res_counts:
+            self.get_top_q(10)
+        top3c = self.u[user_id].argpartition(-3)[-3:]
+
+        # r.um.filled_matrix.loc[10][r.top_q[11]] get rating for the top queries of concept 11 by user 10
+        # r.um.filled_matrix.loc[10][r.top_q[11]].sort_values(ascending=False)[:5].reset_index()["index"]
+        # top 5 queries of concept 11 as rated by user 10
+        top_queries_per_concept = {c: self.um.filled_matrix.loc[user_id][self.top_q[c]].sort_values(
+            ascending=False)[:5].reset_index()["index"] for c in top3c}
+
+        top_res = {c :pd.concat([self.um.dataset.query(self.um.queries[q]) for q in top_queries_per_concept[c]])
+                   for c in top_queries_per_concept}
+
+        res_counts = {i: top_res[i].value_counts()[:5].reset_index() for i in top_queries_per_concept}
+
+        # print(top_res)
+        generators = [generate_query(res_counts[c]) for c in top3c]
+
+        qq, i = list(), 0
+        while len(qq) < length:
+            q = next(generators[i % 3])
+            if q not in qq:
+                qq.append(q)
+            i += 1
         return qq
 
     # TODO: implement "similarity" parameter for user
